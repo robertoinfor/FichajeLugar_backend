1cee9b2180611a56557a6f66dc965f2508e57ea5
robertoinfor <robertoinfor03@gmail.com>
2025-03-03 13:07:45 +0000

    Fichaje eliminacion y editar a medias

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {Client} = require('@notionhq/client');
+const { Client } = require('@notionhq/client');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
@@ -8,64 +8,50 @@ require('dotenv').config();
 
 const app = express();
 app.use(cors());
+app.use(bodyParser.json());
 
 // Añadir como variables de entorno
 const authToken = "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";
-const notionDbID = "1a7879e4f8e480aea957eb64fa533cf3";
-const notion = new Client ({auth: authToken});
+const usuariosDb = "1a7879e4f8e480aea957eb64fa533cf3";
+const fichajeDb = "1a7879e4f8e4800689f7fff9be6f5c1f";
+const notion = new Client({ auth: authToken });
 
-app.post('/NotionAPIPost', jsonParser, async(req, res) => {
-    const {Nombre, Pwd, Email, Rol, Fecha_alta} = req.body;
-    try { 
+app.post('/PostUser', jsonParser, async (req, res) => {
+    const { Nombre, Pwd, Email, Rol, Fecha_alta } = req.body;
+    try {
         const response = await notion.pages.create({
             parent: {
-                database_id: notionDbID,
+                database_id: usuariosDb,
             },
             properties: {
                 Nombre: {
-                    title: [
-                        {
-                            text: {
-                                content: Nombre
-                            },
+                    title: [{
+                        text: {
+                            content: Nombre
                         },
-                    ],
+                    },],
                 },
                 Pwd: {
-                    rich_text: [
-                        {
-                            text: {
-                                content: Pwd
-                            },
+                    rich_text: [{
+                        text: {
+                            content: Pwd
                         },
-                    ],
+                    },],
                 },
                 Email: {
-                    rich_text: [
-                        {
-                            text: {
-                                content: Email
-                            },
-                        },
-                    ],
+                    email: Email
                 },
                 Rol: {
-                    select: [
-                        {
-                            select: {
-                                content: Rol
-                            },
-                        },
-                    ],
+                    select: {
+                        name: Rol
+                    },
+
                 },
                 Fecha_alta: {
-                    rich_text: [
-                        {
-                            text: {
-                                content: Fecha_alta
-                            },
-                        },
-                    ],
+                    date:
+                    {
+                        start: Fecha_alta
+                    },
                 },
             },
         });
@@ -76,10 +62,10 @@ app.post('/NotionAPIPost', jsonParser, async(req, res) => {
     }
 });
 
-app.get('/NotionAPIGet', async(req, res) => {
-    try { 
+app.get('/GetUsers', async (req, res) => {
+    try {
         const response = await notion.databases.query({
-            database_id: notionDbID, 
+            database_id: usuariosDb,
             sorts: [
                 {
                     timestamp: 'created_time',
@@ -89,15 +75,63 @@ app.get('/NotionAPIGet', async(req, res) => {
         });
 
         res.send(response);
-        const {results} = response;
+        const { results } = response;
     } catch (error) {
         console.log(error);
     }
 });
-app.get('/NotionAPIGet', async(req, res) => {
-    try { 
+
+app.delete('/DeleteUser/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const response = await notion.pages.update(
+            {
+                page_id: id,
+                archived: true,
+            }
+        );
+        if (response) {
+            return res.status(200).send({ message: 'Usuario archivado correctamente' });
+        } else {
+            return res.status(400).send({ message: 'Error al archivar el usuario' });
+        }
+    } catch (error) {
+        console.error('Error al hacer la solicitud a Notion:', error);
+        return res.status(500).send({ message: 'Error al eliminar el usuario', error });
+    }
+});
+
+app.put("/UpdateUser/:id", async (req, res) => {
+    const { id } = req.params;
+    const { Nombre, Pwd, Email, Rol, Fecha_alta } = req.body;
+
+    try {
+        const response = await notion.pages.update(
+            {
+                page_id: id,
+            },
+            {
+                properties: {
+                    Nombre: { title: [{ text: { content: Nombre } }] },
+                    Email: { email: Email },
+                    Pwd: { rich_text: [{ text: { content: Pwd }, },], },
+                    Rol: { select: { name: Rol } },
+                    Fecha_alta: { date: { start: Fecha_alta } },
+                },
+            },
+        );
+
+        res.status(200).json({ message: "Usuario actualizado", data: response.data });
+    } catch (error) {
+        console.error("Error actualizando usuario:", error);
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.get('/GetSignings', async (req, res) => {
+    try {
         const response = await notion.databases.query({
-            database_id: notionDbID, 
+            database_id: fichajeDb,
             sorts: [
                 {
                     timestamp: 'created_time',
@@ -107,7 +141,7 @@ app.get('/NotionAPIGet', async(req, res) => {
         });
 
         res.send(response);
-        const {results} = response;
+        const { results } = response;
     } catch (error) {
         console.log(error);
     }
