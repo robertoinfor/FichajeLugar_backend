452da8ff6a6b51b6993194a6fc8c7fef0b7666eb
robertoinfor <robertoinfor03@gmail.com>
2025-03-06 08:59:56 +0000

    Put, encriptar y navegacion

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const jsonParser = bodyParser.json();
 const port = process.env.PORT || 8000;
 require('dotenv').config();
 
+const bcrypt = require('bcrypt');
+
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
@@ -16,9 +18,12 @@ const usuariosDb = "1a7879e4f8e480aea957eb64fa533cf3";
 const fichajeDb = "1a7879e4f8e4800689f7fff9be6f5c1f";
 const notion = new Client({ auth: authToken });
 
+const saltRounds = 10;
+
 app.post('/PostUser', jsonParser, async (req, res) => {
     const { Nombre, Pwd, Email, Rol, Fecha_alta } = req.body;
     try {
+        const hash = await bcrypt.hash(Pwd, saltRounds)
         const response = await notion.pages.create({
             parent: {
                 database_id: usuariosDb,
@@ -34,7 +39,7 @@ app.post('/PostUser', jsonParser, async (req, res) => {
                 Pwd: {
                     rich_text: [{
                         text: {
-                            content: Pwd
+                            content: hash
                         },
                     },],
                 },
@@ -81,6 +86,52 @@ app.get('/GetUsers', async (req, res) => {
     }
 });
 
+app.post('/login', async (req, res) => {
+    const { login, password } = req.body;
+    try {
+        const response = await notion.databases.query({
+            database_id: usuariosDb,
+            filter: {
+                or: [
+                    {
+                        property: "Email",
+                        email: { equals: login }
+                    },
+                    {
+                        property: "Nombre",
+                        title: { contains: login }
+                    }
+                ]
+            }
+        });
+
+        if (response.results.length === 0) {
+            return res.status(404).json({ message: "Usuario no encontrado" });
+        }
+
+        const usuario = response.results[0];
+        const storedPassword = usuario.properties.Pwd.rich_text[0]?.text.content;
+
+        if (storedPassword !== password) {
+            return res.status(401).json({ message: "Contraseña incorrecta" });
+        }
+
+        res.status(200).json({
+            id: usuario.id,
+            nombre: usuario.properties.Nombre.title[0]?.plain_text,
+            email: usuario.properties.Email.email,
+            Pwd: usuario.properties.Pwd.rich_text[0].text.content,
+            rol: usuario.properties.Rol.select.name,
+            fecha_alta: usuario.properties.Fecha_alta.date.start
+        });
+
+    } catch (error) {
+        console.error("Error en login:", error);
+        res.status(500).json({ message: "Error en el servidor", error });
+    }
+});
+
+
 app.delete('/DeleteUser/:id', async (req, res) => {
     const { id } = req.params;
     try {
@@ -109,8 +160,6 @@ app.put("/UpdateUser/:id", async (req, res) => {
         const response = await notion.pages.update(
             {
                 page_id: id,
-            },
-            {
                 properties: {
                     Nombre: { title: [{ text: { content: Nombre } }] },
                     Email: { email: Email },
@@ -120,11 +169,34 @@ app.put("/UpdateUser/:id", async (req, res) => {
                 },
             },
         );
-
         res.status(200).json({ message: "Usuario actualizado", data: response.data });
     } catch (error) {
         console.error("Error actualizando usuario:", error);
         res.status(500).json({ error: error.message });
+    }
+});
+
+app.get('/GetUserName/:name', async (req, res) => {
+    const { name } = req.params;
+    try {
+        const response = await notion.databases.query({
+            database_id: usuariosDb,
+            sorts: [
+                {
+                    timestamp: 'created_time',
+                    direction: 'descending',
+                },
+            ],
+            filter: {
+                property: "Nombre",
+                title: { equals: name }
+            }
+        });
+
+        res.send(response);
+        const { results } = response;
+    } catch (error) {
+        console.log(error);
     }
 });
 
@@ -138,6 +210,30 @@ app.get('/GetSignings', async (req, res) => {
                     direction: 'descending',
                 },
             ]
+        });
+
+        res.send(response);
+        const { results } = response;
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+app.get('/GetSigningUser/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const response = await notion.databases.query({
+            database_id: fichajeDb,
+            sorts: [
+                {
+                    timestamp: 'created_time',
+                    direction: 'descending',
+                },
+            ],
+            filter: {
+                property: "Empleado",
+                relation: { contains: id }
+            }
         });
 
         res.send(response);
