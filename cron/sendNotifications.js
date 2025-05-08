d945ac41bbd03f9a0236e13cb17bf9cde5ada0a2
robertoinfor <robertoinfor03@gmail.com>
2025-05-08 09:24:24 +0100

    Remove .env from version control

--- cron/sendNotifications.js
+++ cron/sendNotifications.js
@@ -1,45 +1,54 @@
 const { CronJob } = require('cron');
-const { notion, db } = require('../utils/notion');
 const { fcm } = require('../firebase');
+const { notion, db } = require('../utils/notion');
 const dayjs = require('dayjs');
 
-
-const job = new CronJob('20 09 * * 1-5', async () => {
+const job = new CronJob(
+  '28 09 * * 1-5',
+  async () => {
     console.log('⏰ Ejecutando recordatorio diario de fichajes…');
+
     const hoy = dayjs().tz('Atlantic/Canary').format('YYYY-MM-DD');
 
     try {
-        const signings = await notion.databases.query({
-            database_id: db.fichajeDb,
-            filter: { property: 'Fecha_hora', date: { equals: hoy } }
-        });
-        const fichados = new Set(signings.results
-            .flatMap(p => p.properties.Empleado.relation.map(r => r.id)));
+      const signings = await notion.databases.query({
+        database_id: db.fichajeDb,
+        filter: { property: 'Fecha_hora', date: { equals: hoy } }
+      });
 
-        const tokensPages = await notion.databases.query({
-            database_id: db.fcmDB
-        });
-        const usuariosConToken = tokensPages.results.map(p => ({
-            userId: p.properties.Empleado.relation[0].id,
-            token: p.properties.Token.title[0].plain_text
-        }));
+      const fichados = new Set(signings.results
+        .flatMap(p => p.properties.Empleado.relation.map(r => r.id)));
 
-        for (const { userId, token } of usuariosConToken) {
-            if (!fichados.has(userId)) {
-                await fcm.send({
-                    token,
-                    notification: {
-                        title: '⏰ ¡No olvides fichar!',
-                        body: 'Aún no has fichado hoy. Regístrate al entrar.'
-                    },
-                    data: { type: 'recordatorio_fichaje' }
-                });
-            }
+      const tokensPages = await notion.databases.query({
+        database_id: db.fcmDB
+      });
+
+      const usuariosConToken = tokensPages.results.map(p => ({
+        userId: p.properties.Empleado.relation[0].id,
+        token: p.properties.Token.title[0].plain_text
+      }));
+
+      for (const { userId, token } of usuariosConToken) {
+        if (!fichados.has(userId)) {
+          await fcm.send({
+            token,
+            notification: {
+              title: '⏰ ¡No olvides fichar!',
+              body: 'Aún no has fichado hoy. Regístrate al entrar.'
+            },
+            data: { type: 'recordatorio_fichaje' }
+          });
         }
-        console.log('Recordatorios enviados.');
+      }
+
+      console.log('Recordatorios enviados.');
     } catch (e) {
-        console.error('Error en cron de recordatorio:', e);
+      console.error('Error en cron de recordatorio:', e);
     }
-});
+  },
+  null,
+  true,
+  'Atlantic/Canary'
+);
 
 job.start();
