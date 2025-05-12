3fcf19a15eef1fa563546a592a9b936071360371
robertoinfor <robertoinfor03@gmail.com>
2025-05-12 12:45:21 +0100

    Commented

--- utils/drive.js
+++ utils/drive.js
@@ -2,7 +2,18 @@ const multer = require('multer');
 const { google } = require('googleapis');
 const stream = require('stream');
 
-const apiCreds = JSON.parse(process.env.API_DRIVE_KEY);
+const apiCreds = {
+  type: process.env.GOOGLE_TYPE,
+  project_id: process.env.GOOGLE_PROJECT_ID,
+  private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+  private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.GOOGLE_CLIENT_EMAIL,
+  client_id: process.env.GOOGLE_CLIENT_ID,
+  auth_uri: process.env.GOOGLE_AUTH_URI,
+  token_uri: process.env.GOOGLE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_CERT,
+  client_x509_cert_url: process.env.GOOGLE_CLIENT_CERT
+};
 
 const storage = multer.memoryStorage();
 exports.uploadMiddleware = multer({ storage });
